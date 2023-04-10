8f0723bcb55cc392c1c5254adb97a8e98b2c0565
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-11 02:07:14 +0530

    initial commit 4

---
+++ src/Components/main/UI/Screens/Home/CardsSection/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardsSection = () => {
+  return (
+    <div>CardsSection</div>
+  )
+}
+
+export default CardsSection
--- src/Components/main/UI/Screens/Home/index.jsx
+++ src/Components/main/UI/Screens/Home/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import HeroSection from "./HeroSection/index"
+import CardsSection from "./CardsSection/index"
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <HeroSection/>
+      <CardsSection/>
+    </>
   )
 }
 
