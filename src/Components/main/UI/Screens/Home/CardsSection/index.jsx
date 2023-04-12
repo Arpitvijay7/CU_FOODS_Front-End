d019d3bedb7fd1ba8d6f0b5b1ed76333700fda76
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-12 17:27:41 +0530

    homepage finished with static data,   1. not connected with backend. 2. card status not set yet.

--- src/Components/main/UI/Screens/Home/CardsSection/index.jsx
+++ src/Components/main/UI/Screens/Home/CardsSection/index.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import Card from "./card";
 
 const CardsSection = () => {
+  const data=[
+    {
+      name:"Punjabi Rasoi",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"Zaika",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"MVR",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"Res3",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"Res4",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"Res5",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"Res6",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    },
+    {
+      name:"Res7",
+      address:"Photo booth fam kinfolk cold-pressed sriracha leggingsjianbing microdosing tousled waistcoat."
+    }
+  ]
   return (
-    <div>CardsSection</div>
-  )
-}
+    <>
+      <div className="w-full -mt-14">
+        <div className="">
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+              <div class="flex flex-wrap -m-4">
 
-export default CardsSection
+                {data && data.map((val,id)=>{
+                  return(
+                    <Card name={val.name} address={val.address} />
+                  )
+                })}
+
+
+
+
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardsSection;
