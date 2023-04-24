c81efcfd2b46a6bce89339dbe8e79133b2e4c4de
Mrinmoy Saikia <108942030+Gzie3009@users.noreply.github.com>
2023-04-24 23:10:09 +0530

    Revert "Homepage 10 mrinmoy"

--- src/Components/main/UI/NavbarComponents/index.js
+++ src/Components/main/UI/NavbarComponents/index.js
@@ -9,7 +9,7 @@ const Navbar = () => {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <span class="ml-3 text-xl">CU FOODS</span>
+      <span class="ml-3 text-xl">Tailblocks</span>
     </a>
     <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
       <a class="mr-5 hover:text-gray-900">First Link</a>
