3e6230776e074c21a22e0d328fa6bece47310f81
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-24 23:43:26 +0530

    added the reset and forgot password page

--- src/Components/main/Routes/Routes.js
+++ src/Components/main/Routes/Routes.js
@@ -5,17 +5,20 @@ import Login from '../UI/Screens/Login'
 import Navbar from "../UI/NavbarComponents/index"
 import Footer from '../UI/FooterComponents'
 import ErrorPage from '../UI/Screens/Error'
+import ForgotPwd from '../UI/Screens/Forgotpassword/forgotPwd'
+import ResetPwd from '../UI/Screens/Resetpassword/resetPwd'
 const Routings = () => {
   const location=useLocation();
     const isHome = location.pathname === '/';
 
     return (
       <>
-        {!isHome && <Navbar />} {/* only render Navbar if not on the home page */}
+        {!isHome && <Navbar />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-
+          <Route path='/resetpassword' element={<ResetPwd/>}/>
+          <Route path='/forgotpassword' element={<ForgotPwd/>}/>
 
 
 
