1c494cbe74ac31b2fff6cdb89639a97ca69f7295
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-09-05 14:42:47 +0530

    Search functionality added in restaurant

--- src/Components/main/UI/Screens/Restaurant/index.js
+++ src/Components/main/UI/Screens/Restaurant/index.js
@@ -1,15 +1,56 @@
-import React from 'react'
-import Banner from './Banner'
-import Menu from "./Menu.js"
+import React, { useState, useEffect } from "react";
+import Banner from "./Banner";
+import Menu from "./Menu.js";
 import { useParams } from "react-router-dom";
+import { BASE_URL } from "../../../../Core/API/endpoint.js";
+import axios from "axios";
 const Restaurant = () => {
-  const {id}=useParams()
+  const { id } = useParams();
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(1);
+  const fetchMenu = async () => {
+    const { data } = await axios(`${BASE_URL}shop/getMenu/${id}`);
+    if (data.message === "Success") {
+      console.log(data.Menu);
+      setData(data.Menu);
+      setLoad(0);
+    }
+  };
+  const handleSearch = async () => {
+    setLoad(true)
+    const res = await fetch(`${BASE_URL}shop/getMenu/${id}?keyword=${search}`);
+    const data = await res.json();
+    if (search.length > 0) {
+      if (data["Menu"].length !== 0) {
+        setData(data.Menu);
+      } else {
+        setData([]);
+      }
+    } else {
+      fetchMenu();
+    }
+    setLoad(0);
+  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    fetchMenu();
+  }, []);
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
   return (
     <>
-        <Banner id={id}/>
-        <Menu id={id}/>
+      <Banner id={id} />
+      <Menu
+        data={data}
+        search={search}
+        setSearch={setSearch}
+        id={id}
+        load={load}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Restaurant
+export default Restaurant;
