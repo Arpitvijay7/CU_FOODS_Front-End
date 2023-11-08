087b274e3e3f248b492a27937b8a88f45a208a1c
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-11-08 07:37:22 +0530

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Routings from './Components/main/Routes/Routes';
-
+import "./App.css";
+import Routings from "./Components/main/Routes/Routes";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 function App() {
+  const location = useLocation(); // If using React Router
+
+  useEffect(() => {
+    // Initialize Google Analytics and track pageview
+    window.gtag("config", "G-VHPMQ72SPJ", { page_path: location.pathname }); // Replace with your Tracking ID
+  }, [location]);
   return (
     <div className="">
-      <Routings/>
+      <Routings />
     </div>
   );
 }
