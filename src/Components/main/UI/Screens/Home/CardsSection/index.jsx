d835397a64bb5af88b5166f876fb160a0404ee55
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2024-02-26 20:09:51 +0530

    skeleton added with animation

--- src/Components/main/UI/Screens/Home/CardsSection/index.jsx
+++ src/Components/main/UI/Screens/Home/CardsSection/index.jsx
@@ -1,15 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../../../../../Core/API/endpoint";
 import Card from "./card";
+import { motion } from "framer-motion";
 const CardsSection = ({ data, setData, load }) => {
   return (
     <>
-      {load ? (
-        <div className="w-screen h-96 grid place-items-center">
+      {!load ? (
+        <div className="w-screen flex pt-10 pb-20 px-2 flex-wrap justify-evenly">
           <div
-            className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-rose-600 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            className="hidden my-10 md:inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-rose-600 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
             role="status"
           ></div>
+          {[1, 2, 3].map((i) => (
+            <motion.div
+              initial={{
+                y: 75,
+                opacity: 0,
+              }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              viewport={{ once: true }}
+              key={i}
+              className="md:hidden animate-pulse h-[30vh] md:h-[25vh] my-2 md:my-2 rounded-3xl w-full md:w-1/2 lg:w-1/3 xl:w-[23vw] bg-neutral-300"
+            />
+          ))}
         </div>
       ) : (
         <>
@@ -18,7 +37,7 @@ const CardsSection = ({ data, setData, load }) => {
               <section className="text-gray-600 body-font">
                 <div className="container pb-8 lg:pb-16">
                   <div className="flex flex-wrap pt-10 -m-4">
-                    {data.length ? (
+                    {data && data.length ? (
                       data.map((val, id) => {
                         return (
                           <Card
@@ -43,7 +62,9 @@ const CardsSection = ({ data, setData, load }) => {
                   </div>
                 </div>
               </section>
-              <h3 className="text-center font-bold">More shops will be added soon...</h3>
+              <h3 className="text-center font-bold mb-20 md:mb-0">
+                More shops will be added soon...
+              </h3>
             </div>
           </div>
         </>
