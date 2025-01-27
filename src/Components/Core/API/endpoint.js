45ec8015bdae0160114c127dcffe5b08e797b847
Arpit Vijay Vergiya <95640748+Arpitvijay7@users.noreply.github.com>
2025-01-27 11:35:03 +0530

    Update endpoint.js

--- src/Components/Core/API/endpoint.js
+++ src/Components/Core/API/endpoint.js
@@ -1,2 +1,4 @@
-export const BASE_URL="https://api.cufoodz.com/api/vi/"
+// export const BASE_URL="https://api.cufoodz.com/api/vi/"
 //export const BASE_URL="http://localhost:4000/api/vi/"
+export const BASE_URL="https://cu-foodz.onrender.com/api/vi"
+
